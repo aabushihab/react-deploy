c7344020c5ae2af2c78a1182a45e676996b9cebb
ammar-Abushihab <aabushihab@ats-ware.com>
2026-07-06 16:14:31 +0300

    fix vercel build1

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,7 @@
 // src/utils/api.js
-const BASE_URL = 'http://localhost:8082'; // replace with your backend URL
+// const BASE_URL = 'http://localhost:8082'; // replace with your backend URL
+//
+const BASE_URL = 'https://web-production-2a6f8.up.railway.app'; 
 
 export const fetchClinicInfo = async () => {
   const response = await fetch(`${BASE_URL}/api/clinic/1/info`);
